62bdc8b9d3e0a0c36df6baa69d44698a01ae0c9f
flameless <flamelessowo@gmail.com>
2023-06-14 15:09:34 +0300

    add search support

--- server/api/views.py
+++ server/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView
 from rest_framework import permissions
+from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Brand, Model, Car
 from .serializers import BrandSerializer, ModelSerializer, CarSerializer
@@ -10,8 +11,9 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 class BrandListView(ListAPIView):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = '__all__'
+    search_fields = ['name']
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
@@ -19,9 +21,10 @@ class BrandListView(ListAPIView):
 class ModelListView(ListAPIView):
     queryset = Model.objects.all()
     serializer_class = ModelSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = ModelFilter
     filterset_fields = '__all__'
+    search_fields = ['name']
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
@@ -29,8 +32,9 @@ class ModelListView(ListAPIView):
 class CarListView(ListAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = CarFilter
     filterset_fields = '__all__'
+    search_fields = ['model__name', 'brand__name']
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication]
