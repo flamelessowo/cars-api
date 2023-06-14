2d5bd1f4bcef2163d2131294ae636bf45f150011
flameless <flamelessowo@gmail.com>
2023-06-14 14:55:45 +0300

    add defend for protected routes
    Assigned authentication class for protected routes.

--- server/api/views.py
+++ server/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.generics import ListAPIView
+from rest_framework import permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Brand, Model, Car
 from .serializers import BrandSerializer, ModelSerializer, CarSerializer
 from .filters import CarFilter, ModelFilter
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class BrandListView(ListAPIView):
@@ -10,6 +12,8 @@ class BrandListView(ListAPIView):
     serializer_class = BrandSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = '__all__'
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
 
 
 class ModelListView(ListAPIView):
@@ -18,6 +22,8 @@ class ModelListView(ListAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_class = ModelFilter
     filterset_fields = '__all__'
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
 
 
 class CarListView(ListAPIView):
@@ -26,3 +32,5 @@ class CarListView(ListAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_class = CarFilter
     filterset_fields = '__all__'
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
